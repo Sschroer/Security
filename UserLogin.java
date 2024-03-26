ea3f70e3067dd2991c8b42b3b403fb8c7b03323a
Sschroer <162058202+Sschroer@users.noreply.github.com>
2024-03-25 23:18:06 -0500

    v3

--- UserLogin.java
+++ UserLogin.java
@@ -1,6 +1,5 @@
 package Security;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -10,96 +9,101 @@ import java.util.Map;
  *
  */
 public class UserLogin {
-	private static Map<String, String> loginInfo = new HashMap<>();
+	private Map<String, String> loginInfo;
 
-	/**
-	 * Prevents instantiation
+	/**Constructor
+	 * @param loginInfo
 	 */
-	private UserLogin() {
+	public UserLogin(Map<String, String> loginInfo) {
+		this.loginInfo = loginInfo;
 	}
 
 	/**
 	 * Checks to see if username is already on map, if not it adds the login
-	 * info.
+	 * info. Need to find a way to prevent
 	 * 
 	 * @param username
 	 * @param password
 	 * @return
 	 */
-	public static boolean addUser(String username, String password) {
-	    if (loginInfo.containsKey(username)) {
-	        System.out.println("The username " + username + " already exists");
-	        return false;
-	    } else {
-	        // Check if password is null or empty
-	        if (password == null || password.isEmpty()) {
-	            System.out.println("Password cannot be null or empty");
-	            return false;
-	        }
-	        System.out.println("User successfully added");
-	        loginInfo.put(username, password);
-	        return true;
-	    }
+	public boolean addUser(String username, String password) {
+		if (loginInfo.containsKey(username)) {
+			System.out.println("The username " + username + " already exists");
+			return false;
+		} else {
+			// Check if password is null or empty
+			if (password == null || password.isEmpty()) {
+				System.out.println("Password cannot be null or empty");
+				return false;
+			}
+			System.out.println("User successfully added");
+			loginInfo.put(username, password);
+			return true;
+		}
 	}
-	
+
 	/**
 	 * Checks to see if username and password are on the map.
+	 * 
 	 * @param username
 	 * @param password
 	 * @return
 	 */
-	public static boolean login(String username, String password) {
-	    if (loginInfo.containsKey(username)) {
-	        if (loginInfo.get(username).equals(password)) {
-	            System.out.println("Login Successful");
-	            return true;
-	        } else {
-	            System.out.println("Login Failed: Incorrect password");
-	            return false;
-	        }
-	    } else {
-	        System.out.println("Login Failed: User not found");
-	        return false;
-	    }
+	public boolean login(String username, String password) {
+		if (loginInfo.containsKey(username)) {
+			if (loginInfo.get(username).equals(password)) {
+				System.out.println("Login Successful");
+				return true;
+			} else {
+				System.out.println("Login Failed: Incorrect password");
+				return false;
+			}
+		} else {
+			System.out.println("Login Failed: User not found");
+			return false;
+		}
 	}
 	/**
 	 * Removes username and password from the user list.
+	 * 
 	 * @param username
 	 * @return
 	 */
-	public static boolean removeUser(String username) {
-		if(loginInfo.containsKey(username)) {
+	public boolean removeUser(String username) {
+		if (loginInfo.containsKey(username)) {
 			loginInfo.remove(username);
 			System.out.println("User removed sucessfully");
 			return true;
-		}else {
+		} else {
 			System.out.println("User not found");
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Resets pasword is user exists.
+	 * 
 	 * @param username
 	 * @param oldPass
 	 * @param newPass
 	 * @return
 	 */
-	public static boolean editPassword(String username, String oldPass, String newPass) {
-	    if (!loginInfo.containsKey(username)) {
-	        System.out.println("User not found");
-	        return false;
-	    }
+	public boolean editPassword(String username, String oldPass,
+			String newPass) {
+		if (!loginInfo.containsKey(username)) {
+			System.out.println("User not found");
+			return false;
+		}
 
-	    if (loginInfo.get(username).equals(oldPass)) {
-	        loginInfo.put(username, newPass);
-	        System.out.println("Password successfully reset");
-	        return true;
-	    }
+		if (loginInfo.get(username).equals(oldPass)) {
+			loginInfo.put(username, newPass);
+			System.out.println("Password successfully reset");
+			return true;
+		}
 
-	    // If old password doesn't match
-	    System.out.println("Incorrect old password");
-	    return false;
+		// If old password doesn't match
+		System.out.println("Incorrect old password");
+		return false;
 	}
 }
 
--- UserLoginTest.java
+++ UserLoginTest.java
@@ -3,76 +3,89 @@ package Security;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class UserLoginTest {
-    static String userOne = "UserOne";
-    static String userOnePass = "12345AbC";
-    static String userTwo = "UserTwo";
-    static String userTwoPass = "cBa54321!";
-    static String userThree = "NonExistentUser";
+	static String userOne = "";
+	static String userOnePass = "";
+	static String userTwo = "UserTwo";
+	static String userTwoPass = "cBa54321!";
+	static String userThree = "NonExistentUser";
+	static Map<String,String> loginInfo = new HashMap<>();
+	static UserLogin login = new UserLogin(loginInfo);
 
-    @BeforeAll
-    public static void setUp() {
-        UserLogin.addUser(userOne, userOnePass);
-    }
+	@BeforeAll
+	public static void setUp() {
+		userOne = "UserOne";
+	    userOnePass = "12345AbC";
+		login.addUser(userOne, userOnePass);
+		
+	}
 
-    @Test
-    void addUserPass() {
-        assertTrue(UserLogin.addUser(userTwo, userTwoPass));
-    }
-    
-    @Test 
-    void addUserFail() {
-    	UserLogin.addUser(userOne, userOnePass);
-    	assertFalse(UserLogin.addUser(userOne, userOnePass));
-    }
-    
-    @Test
-    void addUserFailPassword() {
-    	assertFalse(UserLogin.addUser(userThree, ""));
-    }
-    
-    @Test
-    void loginTestPass() {
-    	assertTrue(UserLogin.login(userOne, userOnePass));
-    }
-    
-    @Test
-    void loginTestFailUserError() {
-    	assertFalse(UserLogin.login(userThree, userOnePass));
-    }
-    
-    @Test
-    void loginTestFailPasswordError() {
-    	assertFalse(UserLogin.login(userOne, userTwoPass));
-    }
-    
-    @Test
-    void removeUserPass() {
-    	assertTrue(UserLogin.removeUser(userOne));
-    }
-    
-    @Test
-    void removeUserFail() {
-    	assertFalse(UserLogin.removeUser(userThree));
-    }
-    
-    @Test
-    void testSuccessfulPasswordReset() {
-        assertTrue(UserLogin.editPassword("UserOne", "12345AbC", "newPassword"));
-    }
+	@Test
+	void addUserPass() {
+		assertTrue(login.addUser(userTwo, userTwoPass));
+	}
 
-    @Test
-    void testUserNotFound() {
-        assertFalse(UserLogin.editPassword("NonExistentUser", "oldPassword", "newPassword"));
-      
-    }
+	@Test
+	void addUserFail() {
+		login.addUser(userOne, userOnePass);
+		assertFalse(login.addUser(userOne, userOnePass));
+	}
 
-    @Test
-    void testIncorrectOldPassword() {
-        assertFalse(UserLogin.editPassword(userOne, "wrongPassword", "newPassword"));
-       
-    }
+
+	@Test
+	void addUserFailPassword() {
+		assertFalse(login.addUser(userThree, ""));
+	}
+
+	@Test
+	void loginTestPass() {
+		assertTrue(login.login(userOne, userOnePass));
+	}
+
+	@Test
+	void loginTestFailUserError() {
+		assertFalse(login.login(userThree, userOnePass));
+	}
+
+	@Test
+	void loginTestFailPasswordError() {
+		assertFalse(login.login(userOne, userTwoPass));
+	}
+
+	@Test
+	void removeUserPass() {
+		assertTrue(login.removeUser(userOne));
+	}
+
+	@Test
+	void removeUserFail() {
+		assertFalse(login.removeUser(userThree));
+	}
+
+	@Test
+	void testSuccessfulPasswordReset() {
+		assertTrue(
+				login.editPassword("UserOne", "12345AbC", "newPassword"));
+	}
+
+	@Test
+	void testUserNotFound() {
+		assertFalse(login.editPassword("NonExistentUser", "oldPassword",
+				"newPassword"));
+
+	}
+
+	@Test
+	void testIncorrectOldPassword() {
+		assertFalse(login.editPassword(userOne, "wrongPassword",
+				"newPassword"));
+
+	}
 }
+
